cbfabf9ace2b1dc97cf2324bdd7e648d33379ba9
hagguishel <hagguirzt@gmail.com>
2024-11-25 11:58:56 +0100

    add prototype

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_decimal(va_list args);
+int print_int(va_list args);
 
 #endif
