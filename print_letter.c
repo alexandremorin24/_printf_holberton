1d88830b66b224d9e8b6a3aae043cd53d8124326
hagguishel <hagguirzt@gmail.com>
2024-11-26 13:53:53 +0100

    add letter

--- print_letter.c
+++ print_letter.c
@@ -11,3 +11,27 @@ int print_char(va_list args)
     char c = va_arg(args, int);
     return (_putchar(c));
 }
+
+#include "main.h"
+
+/**
+ * print_string - Function to print a string
+ * @args: va_list containing the argument of type char *
+ *
+ * Return: The number of characters printed
+ */
+int print_string(va_list args);
+{
+    char *str = va_arg(args, char *)
+    int count = 0;
+
+    if (str == NULL)
+    {
+        str = "(null)";
+    }
+    for (; *str != '\0'; str++)
+    {
+        count += _putchar(*str);
+    }
+    return (count);
+}
