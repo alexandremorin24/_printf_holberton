66dc6b488a7ad130612c90a10ad0aa899406e51c
Alexandre <9761@holbertonstudents.com>
2024-11-27 15:45:23 +0100

    Retrait d'un commentaire inutile

--- utils.c
+++ utils.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string
- * @s: Pointer to the string to measure
- *
- * Description: This function traverses the string until it reaches
- * the null character to count the number of characters.
- *
- * Return: The length of the string
- */
-
-int _strlen(char *s)
-{
-int count = 0;
-while (s[count] != '\0')
-{
-count++;
-}
-return (count);
-}
-
-#include <unistd.h>
-
-/**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
